5cbb97c745a8e55874c1498cb435d0a94c701adf
Ian Jang <siabard@gmail.com>
2017-05-11 21:58:53 +0900

    property directive

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,7 +8,14 @@ export class ServerComponent {
     serverId : number = 10;
     serverStatus : string = 'offline';
 
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
     public getServerStatus() : string {
         return this.serverStatus;
+    }
+
+    public getColor() : string {
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 };
